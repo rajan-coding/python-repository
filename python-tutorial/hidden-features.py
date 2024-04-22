d4a304c1e5618702a01dc4dc145e65d748b675fc
Rajamanickam <rajan.gitcode@gmail.com>
2024-04-21 23:01:17 -0700

    hidden features - while else if else

--- python-tutorial/hidden-features.py
+++ python-tutorial/hidden-features.py
@@ -16,3 +16,30 @@ x, _ , z=large_coordinate
 second_elements=[ y for  _ , y in list_of_pairs]
 
 print(second_elements)
+
+# while else, for else
+
+
+items =["a","b","c","d","e","f"]
+i=0
+
+while i < len(items):
+    item = items[i]
+
+    if items =="z":
+        print("found it")
+        break
+    
+    i +=1
+else:
+    print("not found")
+
+for item in items:
+    
+    if items =="z":
+        print("found it")
+        break
+    
+    i +=1
+else:
+    print("not found")
